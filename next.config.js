dbfb9d3bf359a5a4a31d7f9d65c3dcc27af0c680
Shawqi Hatem <shawqicauses@outlook.com>
2024-11-18 01:37:09 +0200

    Update: Enhance through updating `next` version

--- next.config.js
+++ next.config.js
@@ -1,6 +1,23 @@
-// DONE REVIEWING: GITHUB COMMIT
+// DONE REVIEWING: GITHUB COMMIT - 01
 /** @type {import("next").NextConfig} */
 
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "3000",
+        pathname: "**"
+      },
+      {
+        protocol: "https",
+        hostname: process.env.NEXT_PUBLIC_APPLICATION_DOMAIN,
+        pathname: "**"
+      }
+    ]
+  }
+}
 
 module.exports = nextConfig
