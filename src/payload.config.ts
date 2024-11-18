ab5039ae7ead3d0d6f3e77df1f5d8ad6a2a793e2
Shawqi Hatem <shawqicauses@outlook.com>
2024-11-18 22:10:42 +0200

    New: Enhance through adding `payload.config.ts` file

---
+++ src/payload.config.ts
@@ -0,0 +1,18 @@
+// DONE REVIEWING: GITHUB COMMIT
+import {webpackBundler} from "@payloadcms/bundler-webpack"
+import {mongooseAdapter} from "@payloadcms/db-mongodb"
+import {slateEditor} from "@payloadcms/richtext-slate"
+import dotenv from "dotenv"
+import path from "path"
+import {buildConfig} from "payload/config"
+
+dotenv.config({path: path.resolve(__dirname, "../.env")})
+
+export default buildConfig({
+  serverURL: process.env.NEXT_PUBLIC_APPLICATION_URL as string,
+  db: mongooseAdapter({url: process.env.DATABASE_URI as string}),
+  typescript: {outputFile: path.resolve(__dirname, "payload-types.ts")},
+  admin: {bundler: webpackBundler()},
+  editor: slateEditor({}),
+  collections: []
+})
